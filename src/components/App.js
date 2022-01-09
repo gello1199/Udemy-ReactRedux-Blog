8a07f9d544844221c41323ce657f85d8ac9ecaf8
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-09 12:16:13 -0500

    Add semantic ui to html file

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Postlist from './PostList';
 
 const App = () => {
     return (
         <div className="ui container">
-            App
+            <Postlist />
         </div>
     );
 }
