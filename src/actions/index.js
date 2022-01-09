c29379317ef9b375cfd10272a22ce5d7e9d24b25
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-09 13:46:04 -0500

    Connect thunk to store in index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,10 @@
-// import jsonPlaceholder from "../apis/jsonPlaceholder"
+import jsonPlaceholder from "../apis/jsonPlaceholder"
 
 export const fetchPosts = () => {
+    const promise = jsonPlaceholder.get('/posts')
+
     return {
-        type: 'FETCH_POSTS'
+        type: 'FETCH_POSTS',
+        payload: promise
     }
 }
