7ae4e42af43440393623c25f5d05bdea990c408f
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-10 14:42:41 -0500

    Add switch statement to postsReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
-import PostsReducer from "./PostsReducer";
+import postsReducer from "./postsReducer";
 
 export default combineReducers({
-    posts: PostsReducer
+    posts: postsReducer
 })
